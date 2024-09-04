304364af73c3372ed35be23e05a41dca8509bc93
Parth Kikani <parthkikani02@gmail.com>
2024-09-04 13:05:44 +0300

    complete section 9

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -6,12 +6,19 @@ import yelp from '../api/yelp';
 const SearchScreen = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
 
   const searchApi = async () => {
-    const response = await yelp.get('/search', {
-      params: { limit: 50, term: searchTerm, location: 'san jose' },
-    });
-    setResults(response.data.businesses);
+    try {
+      const response = await yelp.get('/search', {
+        params: { limit: 50, term: searchTerm, location: 'san jose' },
+      });
+      setResults(response.data.businesses);
+      setErrorMessage('');
+    } catch (error) {
+      console.log(error);
+      setErrorMessage('Something went wrong');
+    }
   };
 
   return (
@@ -21,7 +28,7 @@ const SearchScreen = () => {
         onSearchTermChange={(newSearchTerm) => setSearchTerm(newSearchTerm)}
         onSearchTermSubmit={() => searchApi()}
       />
-      <Text>Search Screen</Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>Found: {results.length}</Text>
     </View>
   );
