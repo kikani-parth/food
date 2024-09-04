9b79fa0b57fb2b3a8bead98549bb037e896d1169
Parth Kikani <parthkikani02@gmail.com>
2024-09-04 12:24:32 +0300

    add some styling and props to the SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,22 +1,42 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, TextInput, StyleSheet } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
-const SearchBar = () => {
+const SearchBar = ({ searchTerm, onSearchTermChange, onSearchTermSubmit }) => {
   return (
-    <View style={styles.background}>
-      <Feather name="search" size={30} />
-      <Text>Search Bar</Text>
+    <View style={styles.backgroundStyle}>
+      <Feather name="search" style={styles.iconStyle} />
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.inputStyle}
+        placeholder="Search"
+        value={searchTerm}
+        onChangeText={(newSearchTerm) => onSearchTermChange(newSearchTerm)}
+        onEndEditing={() => onSearchTermSubmit()}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  background: {
+  backgroundStyle: {
     backgroundColor: 'rgb(230, 228, 228)',
     height: 50,
     borderRadius: 5,
-    margin: 15,
+    marginHorizontal: 15,
+    marginTop: 10,
+    flexDirection: 'row',
+    justifyContent: '',
+  },
+  inputStyle: {
+    flex: 1,
+    fontSize: 18,
+  },
+  iconStyle: {
+    fontSize: 30,
+    alignSelf: 'center',
+    marginHorizontal: 15,
   },
 });
 
