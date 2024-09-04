6febf21fa324c7cce5f5ae283c0741eed23d7a94
Parth Kikani <parthkikani02@gmail.com>
2024-09-04 12:48:14 +0300

    configure 'axios' to use 'yelp' api

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [results, setResults] = useState([]);
+
+  const searchApi = async () => {
+    const response = await yelp.get('/search', {
+      params: { limit: 50, term: searchTerm, location: 'san jose' },
+    });
+    setResults(response.data.businesses);
+  };
+
   return (
     <View>
-      <SearchBar />
+      <SearchBar
+        searchTerm={searchTerm}
+        onSearchTermChange={(newSearchTerm) => setSearchTerm(newSearchTerm)}
+        onSearchTermSubmit={() => searchApi()}
+      />
       <Text>Search Screen</Text>
+      <Text>Found: {results.length}</Text>
     </View>
   );
 };
